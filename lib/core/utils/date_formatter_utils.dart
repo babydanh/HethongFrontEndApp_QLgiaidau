0b891eb7ad481581c9e081405bff268cbf300ad7
babydanh <Macter.970@gmail.com>
2026-08-19 21:36:52 +0700

    fix(app): ensure DateFormatterUtils formats DateTime with local device timezone

--- lib/core/utils/date_formatter_utils.dart
+++ lib/core/utils/date_formatter_utils.dart
@@ -2,22 +2,22 @@ import 'package:intl/intl.dart';
 
 class DateFormatterUtils {
   static String formatDateTime(DateTime date) {
-    return DateFormat('dd/MM/yyyy HH:mm').format(date);
+    return DateFormat('dd/MM/yyyy HH:mm').format(date.toLocal());
   }
 
   static String formatTime(DateTime time) {
-    return DateFormat('HH:mm').format(time);
+    return DateFormat('HH:mm').format(time.toLocal());
   }
 
   static String formatTimeWithSeconds(DateTime time) {
-    return DateFormat('HH:mm:ss').format(time);
+    return DateFormat('HH:mm:ss').format(time.toLocal());
   }
 
   static String formatDate(DateTime date) {
-    return DateFormat('dd/MM/yyyy').format(date);
+    return DateFormat('dd/MM/yyyy').format(date.toLocal());
   }
 
   static String formatFileTime(DateTime date) {
-    return DateFormat('yyyyMMdd').format(date);
+    return DateFormat('yyyyMMdd').format(date.toLocal());
   }
 }
