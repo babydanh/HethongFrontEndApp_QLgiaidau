fa2cae926b45e046bc82997263ff9c43e3051db6
babydanh <babydanh@gmail.com>
2026-09-22 15:42:34 +0700

    fix(router): remove unsupported canScoreMatch parameter from BracketViewScreen

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -402,7 +402,6 @@ final routerProvider = Provider<GoRouter>((ref) {
                 tournamentId: tournamentId,
                 divisionId: state.uri.queryParameters['divisionId'],
                 canEditBracket: true,
-                canScoreMatch: true,
               );
             },
           ),
